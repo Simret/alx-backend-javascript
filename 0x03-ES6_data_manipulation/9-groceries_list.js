e117ea377b0b143558c2d3dd4eb3922e701f1b2b
simret <simruineb@gmail.com>
2023-02-01 05:47:39 +0300

    Map

---
+++ 0x03-ES6_data_manipulation/9-groceries_list.js
@@ -0,0 +1,12 @@
+const groceriesList = () => {
+  const gcmap = new Map();
+
+  gcmap.set('Apples', 10);
+  gcmap.set('Tomatoes', 10);
+  gcmap.set('Pasta', 1);
+  gcmap.set('Rice', 1);
+  gcmap.set('Banana', 5);
+  return gcmap;
+};
+
+export default groceriesList;
